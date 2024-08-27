82e8a397137a784f0a542e115a0df873482f5499
karemhamam <56442491+karemhamam@users.noreply.github.com>
2024-08-27 18:02:57 +0300

    hhm.c

--- hmm_2/hhm.c
+++ hmm_2/hhm.c
@@ -1,11 +1,11 @@
-/* *******************
+/* *******
  * @File                 : hhm.c
  * @Author               : Mohamed Bahget Hamam
  * @gmail                : mahamedhamam15@gmail.com
- *******************
+ *******
 */
 
-/* ****** Include section start ***** */
+/* ******** include section start *********** */
 
 #include <stdint.h>                                         // includes the standard integer types header.
 #include <stdio.h>                                          // includes the standard input/output header.
@@ -14,16 +14,16 @@
 #include <errno.h>                                          // for errno.
 #include "hhm.h"                                            // includes the header file which has function declarations.
 
-/* ****** Include section End ***** */
+/* ******** Include section End *********** */
 
-/* ****** macros section start ***** */
+/* ******** macros section Start *********** */
 
-#define HEAP_SIZE (1024 * 1024)                               // 1 MB heap size.
+#define HEAP_SIZE (1024 * 1024)                             // 1 MB heap size.
 #define BLOCK_HEADER_SIZE sizeof(block_header)              // define the size of block header.
 
-/* ****** macros section End ***** */
+/* ******** macros section End *********** */
 
-/* ****** Definition section start ***** */
+/* ******* Definition section start ******* */
 
 typedef struct block_header                                 // structure to manage memory blocks.
 {
@@ -43,12 +43,12 @@ static void init_heap()                                      // initializes the 
         heap_start = (char *)sbrk(HEAP_SIZE);                 // expand the heap by HEAP_SIZE using sbrk().
         if (heap_start == (void *)-1)                         // check for errors.
         {
-            perror("error sbrk");
+            perror("sbrk error");
             _exit(1);                                         // terminate the program.
         }
         
-        heap_end = heap_start + HEAP_SIZE ;                    // set the end of the heap.
-        current_brk = heap_start ;                             // set the current end of the heap.
+        heap_end = heap_start + HEAP_SIZE;                    // set the end of the heap.
+        current_brk = heap_start;                             // set the current end of the heap.
     }
 }
 
@@ -65,7 +65,7 @@ static block_header* find_free_block(size_t size)            // finds a free blo
 
 void *HmmAlloc(size_t size)                                  // allocates a block of memory.
 {
-    if (size == 0) return NULL ;                             // if size = 0 return NULL.
+    if (size == 0) return NULL;                              // if size = 0 return NULL.
 
     if (heap_start == NULL)
     {
@@ -78,20 +78,31 @@ void *HmmAlloc(size_t size)                                  // allocates a bloc
 
     if (block)                                               // suitable block found?
     {
-        block->free = 0 ;                                    // mark it as used.
+        /* block splitting */
+        if (block->size > size + BLOCK_HEADER_SIZE)
+        {
+            block_header *new_block = (block_header *)((char *)block + BLOCK_HEADER_SIZE + size);
+            new_block->size = block->size - size - BLOCK_HEADER_SIZE;
+            new_block->free = 1;
+            new_block->next = block->next;
+
+            block->size = size;
+            block->next = new_block;
+        }
+        block->free = 0;                                     // mark it as used.
         return (void *)(block + 1);                          // return pointer to the data part.
     }
 
     if (current_brk + size + BLOCK_HEADER_SIZE > heap_end)  // do you need more memory?
     {
-        size_t expand_size = size + (heap_end - heap_start); // amount of memory to expand.
+        size_t expand_size = size + BLOCK_HEADER_SIZE;
 
         if (sbrk(expand_size) == (void *)-1)                 // expand using sbrk() and check for errors.
         {
-            perror("error sbrk");
+            perror("sbrk error");
             return NULL;
         }
-        heap_end = heap_start + expand_size;                 // update the end of the heap.
+        heap_end += expand_size;                             // update the end of the heap.
     }
 
     block = (block_header *)current_brk;                     // allocate a new block.
@@ -99,88 +110,106 @@ void *HmmAlloc(size_t size)                                  // allocates a bloc
     block->free = 0;                                         // mark block as used.
     block->next = NULL;                                      // initialize the next block pointer.
 
+    if (heap_start == (char *)block)
+    {
+        heap_start = (char *)block;
+    } else 
+    {
+        block_header *current = (block_header *)heap_start;
+        while (current->next != NULL)
+        {
+            current = current->next;
+        }
+        current->next = block;
+    }
+
     current_brk += size + BLOCK_HEADER_SIZE;                 // update the current end of the heap.
     return (void *)(block + 1);                              // return pointer to the data part of the block.
 }
 
-void HmmFree(void *ptr)                                    // frees a block of memory.
+void HmmFree(void *ptr)                                      // frees a block of memory.
 {
-    if (ptr == NULL) return;                                // return if the pointer is NULL.
+    if (ptr == NULL) return;                                 // return if the pointer is NULL.
 
-    block_header *block = (block_header *)ptr - 1;          // calculate the block header address.
-    block->free = 1;                                        // mark the block as free.
+    block_header *block = (block_header *)ptr - 1;           // calculate the block header address.
+    block->free = 1;                                         // mark the block as free.
 
-    /* merge adjacent free blocks. */
-    block_header *current = (block_header *)heap_start;     // start from the beginning of the heap.
-    while (current)                                         // iterate through the blocks.
+    printf("freed block at address %p with size %zu \n", (void *)block , block->size);
+
+    /* merge adjacent free blocks */
+    block_header *current = (block_header *)heap_start;      // start from the beginning of the heap.
+    while (current && current->next)                         // iterate through the blocks.
     {
-        block_header *next = current->next;                 // get the next block.
-
-        if (current->free && next && next->free)            // check if the current and next are free.
+        if (current->free && current->next->free)            // check if the current and next are free.
         {
-            current->size += next->size + BLOCK_HEADER_SIZE;  // merge the current and the next block.
-            current->next = next->next;                     // update the next pointer.
+            current->size += current->next->size + BLOCK_HEADER_SIZE;  // merge the current and the next block.
+            current->next = current->next->next;             // update the next pointer.
+
+            printf("Merged block at %p with next block\n", (void *)current);
         } else 
         {
-            current = next;                                 // move to the next block.
+            current = current->next;                         // move to the next block.
         }
     }
 }
 
-void *HmmRealloc(void *ptr, size_t size)                    // reallocates a block of memory.
+void *HmmRealloc(void *ptr, size_t size)                     // reallocates a block of memory.
 {
     if (ptr == NULL) return HmmAlloc(size);                  // allocate a new block if ptr is NULL.
 
-    block_header *old_block = (block_header *)ptr - 1;      // calculate the address of the old block header.
+    block_header *old_block = (block_header *)ptr - 1;       // calculate the address of the old block header.
+
+    size = (size + sizeof(size_t) - 1) & ~(sizeof(size_t) - 1);
 
     if (size <= old_block->size) return ptr;                 // return the old pointer if the new size is less than or equal to the old size.
 
-    void *new_ptr = HmmAlloc(size);                           // allocate a new block.
-    if (new_ptr == NULL) return NULL ;                        // return NULL if allocation fails.
+    void *new_ptr = HmmAlloc(size);                          // allocate a new block.
+    if (new_ptr == NULL) return NULL;                        // return NULL if allocation fails.
 
-    memcpy(new_ptr, ptr, old_block->size);                    // copy data from the old block to the new block.
-    HmmFree(ptr);                                             // free the old block.
+    memcpy(new_ptr, ptr, old_block->size);                   // copy data from the old block to the new block.
+    HmmFree(ptr);                                            // free the old block.
 
-    return new_ptr;                                         // return the new pointer.
+    return new_ptr;                                          // return the new pointer.
 }
 
 void *HmmCalloc(size_t num, size_t size)                     // allocates and zero-initializes memory.
 {
     void *ptr = HmmAlloc(num * size);                        // allocate memory.
-    if (ptr)                                                // allocation success? Zero-initialize memory.
+    if (ptr)                                                 // allocation success? Zero-initialize memory.
     {
-        memset(ptr, 0, num * size);                         // set the allocated memory to zero.
+        memset(ptr, 0, num * size);                          // set the allocated memory to zero.
     }
 
-    return ptr;                                             // return the pointer.
+    return ptr;                                              // return the pointer.
 }
 
 /* Wrapping libc functions */
 
-void *malloc(size_t size)                                  // Wraps HmmAlloc to provide a standard malloc interface.
+void *malloc(size_t size)                                    // Wraps HmmAlloc to provide a standard malloc interface.
 {
     return HmmAlloc(size);
 }
 
-void free(void *ptr)                                      // Wraps HmmFree to provide a standard free interface.
+void free(void *ptr)                                         // Wraps HmmFree to provide a standard free interface.
 {
     HmmFree(ptr);
 }
 
-void *realloc(void *ptr, size_t size)                      // Wraps HmmRealloc to provide a standard realloc interface.
+void *realloc(void *ptr, size_t size)                        // Wraps HmmRealloc to provide a standard realloc interface.
 {
     return HmmRealloc(ptr, size);
 }
 
-void *calloc(size_t num, size_t size)                      // Wraps HmmCalloc to provide a standard calloc interface.
+void *calloc(size_t num, size_t size)                        // Wraps HmmCalloc to provide a standard calloc interface.
 {
     return HmmCalloc(num, size);
 }
 
-/* ***** Definition section End ****** */
+/* *********** Definition section End ******** */
 
-/* *******************
+/* **********************************************
     User                Date               Time
-   *******************
+   ***********************************************
    Mohamed Hamam       20 Aug 2024        02:54 PM
+   Mohamed Hamam       27 aug 2024        05:54 PM
 */
